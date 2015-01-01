e8dca511c26388b40f8b69d884bcecff65b948d7
Derek Gottlieb <derek.gottlieb@gmail.com>
2015-01-01 16:09:11 -0600

    Tweaked scrub check

--- zpool-scrub-check.sh
+++ zpool-scrub-check.sh
@@ -12,12 +12,10 @@ do
   RUNNING=$(zpool status $pool | grep "scan:" | grep -c "in progress")
   if [[ $RUNNING -gt 0 ]]; then
     echo "Scrub for $pool still running... $(zpool status $pool | grep 'to go')"
-    #zpool status $pool | grep "to go"
     continue
   fi
 
-  #ERRORS=$(zpool status $pool | grep "scan:" | grep -v "in progress" | grep -v " 0 errors" | wc -l)
-  ERRORS=$(zpool status $pool | grep "scan:" | grep -v " 0 errors" | wc -l)
+  ERRORS=$(zpool status $pool | grep "scan:" | grep -c -v "with 0 errors")
   if [[ $ERRORS -gt 0 ]]; then
     echo "Scrub detected errors in $pool!"
   fi
